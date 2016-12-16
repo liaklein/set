876b58bb4a48c70b5f0e9e4819048ae254465fcf
J. Hassler Thurston <jthurst3@u.rochester.edu>
2016-12-16 01:13:44 -0500

    Minor changes: - add objects.pyc to gitignore - fix bug in testing whether something's a set - validate user input

--- game.py
+++ game.py
@@ -1,12 +1,13 @@
 from objects import *
 
 def validSet(card1,card2,card3):
+    print('\n\t\t' + str(card1) +  '\t\t' + str(card2) + '\t\t' + str(card3) + '\n')
     for att in card1.attributes:
         att1 = card1.attributes[att]
         att2 = card2.attributes[att]
         att3 = card3.attributes[att]
         print att1 + ' ' + att2 + ' ' + att3
-        if not(allSame(att1,att2,att2) or allDifferent(att1,att2,att3)):
+        if not(allSame(att1,att2,att3) or allDifferent(att1,att2,att3)):
             return False
     return True
 def allSame(att1,att2,att3):
@@ -19,16 +20,38 @@ def allDifferent(att1,att2,att3):
         return True
     else:
         return False
+
+def validateInput(userInput):
+    cards = userInput.split(' ')
+    if not cards or len(cards) != 3:
+        print('Invalid input')
+        return
+    # try to convert input to integers
+    intCards = []
+    for card in cards:
+        try:
+            intCard = int(card)
+            intCards.append(intCard)
+        except ValueError:
+            print('Invalid input')
+            return
+    # numbers should be between 0 and 11 inclusive, no duplicates
+    intCards.sort()
+    if intCards[0] < 0 or intCards[2] > 11 or intCards[0] == intCards[1] or intCards[1] == intCards[2]:
+        print('Invalid input')
+        return
+    return intCards
+
 def main():
     score = 0
     board = Board()
     while not board.isEmpty():
         board.display()
-        userInput = raw_input("\n\nPlease Enter Set in space separated list: ")
-        c1,c2,c3 = userInput.split(' ')
-        c1 = int(c1)
-        c2 = int(c2)
-        c3 = int(c3)
+        userInput = raw_input("\n\nPlease Enter Set in space separated list (e.g. 0 10 4): ")
+        cards = validateInput(userInput)
+        if not cards:
+            continue
+        c1,c2,c3 = cards
         if validSet(board.cards[c1],board.cards[c2],board.cards[c3]):
             print 'nice set'
             board.replaceIndex(c1)
